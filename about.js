e7cb6417426f9a8909f1135bd5241f687bd2dfa7
Vincent <92563579+jkirv@users.noreply.github.com>
2023-10-29 01:27:06 +0530

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+let shirtParagraph = document.getElementById("shirt")
+console.log(shirt)
+
+
+function shirt(){
+    console.log("button clicked")
+    shirtParagraph.textContent="VISA | MASTERCARD | ATMCARD | PayPal"
+
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+let paymentParagraph = document.getElementById("payment")
+console.log(payment)
+
+
+function purchase(){
+    console.log("button clicked")
+    paymentParagraph.textContent="VISA | MASTERCARD | ATMCARD | PayPal"
+
+}
+
+
+
+
+
